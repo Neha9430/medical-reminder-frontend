65aafc2513a64fb697df7291b74e8aa67f7c0239
Neha9430 <nehamakashre70@gmail.com>
2025-07-24 10:19:11 +0530

    Fixed backend API URL

--- src/config.js
+++ src/config.js
@@ -1,4 +1,6 @@
 // config.js
-const API_BASE_URL = "https://medical-reminder-backend.onrender.com/api";
+// ✅ Correct
+const API_BASE_URL = "https://medical-prescription-reminder.onrender.com/api";
+
 
 export default API_BASE_URL;
